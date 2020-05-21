d1be0c462357b99a7fdea02305bd8b64582f4c69
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-05-21 19:50:15 +0200

    Updates

---
+++ precache-manifest.b7cff644f6c654db3f72cc9f983cece3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9296e0f096de53d56b6c1e98d50575c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15df0cc6bfee787262cb",
+    "url": "/portfolio/static/css/main.543d64da.chunk.css"
+  },
+  {
+    "revision": "dc91d5a99ce2293a319f",
+    "url": "/portfolio/static/js/2.549c21be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.549c21be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15df0cc6bfee787262cb",
+    "url": "/portfolio/static/js/main.b60420f7.chunk.js"
+  },
+  {
+    "revision": "6ebaaf8cc32fd57c0e0b",
+    "url": "/portfolio/static/js/runtime-main.cc1e1739.js"
+  },
+  {
+    "revision": "ffa31b66fcb1fd6cacb82798f46e2e78",
+    "url": "/portfolio/static/media/Dmitriy2020_circle.ffa31b66.png"
+  },
+  {
+    "revision": "d2ec9ff0f6ab6dd0121a57924ef1b648",
+    "url": "/portfolio/static/media/arrow.d2ec9ff0.svg"
+  },
+  {
+    "revision": "5b2a5d4820fc495ae9e440550f71bbe5",
+    "url": "/portfolio/static/media/chat.5b2a5d48.svg"
+  },
+  {
+    "revision": "3306b8d7d257eef585378b1bae63c08a",
+    "url": "/portfolio/static/media/medal.3306b8d7.svg"
+  },
+  {
+    "revision": "94d93db28bacd95d23cf6f6c70715966",
+    "url": "/portfolio/static/media/space_bg.94d93db2.jpeg"
+  }
+]);

016df95cfca7766135c2bc3527d0671511d84db6
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-06-25 10:30:38 +0200

    +gsap

--- src/components/section_1/Section_1.js
+++ src/components/section_1/Section_1.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { Component, useEffect, useRef, useState, useMemo }from 'react'
 import s from './Section_1.module.css'
+import SplitText from './../../js/SplitText'
+import {gsap, TimelineLite} from "gsap";
 
 
 import chatIcon from '../../assets/imgs/chat.svg'
 import arrowIcon from '../../assets/imgs/arrow.svg'
 
 
-function Section_1() {
+class Section_1 extends Component {
+
+    componentDidMount() {
+        
+        let animateText = new TimelineLite(),
+            mySplitText = new SplitText("#intro", { type: "words,chars" }),
+            chars = mySplitText.chars;
+
+        gsap.set("#intro", { perspective: 400 });
+        animateText.staggerFrom(chars, 1, { duration: 0.6, scale: 4, autoAlpha: 0, rotationX: -180, transformOrigin: "100% 50%", ease: "back.out", stagger: 0.02 });
+
+        gsap.from("#action_btn", 1, { delay: 1, opacity: 0, x: -100 });
+        gsap.from("#arrow", .5, { delay: 2, opacity: 0, y: 30});
+    }
+    
+   
+    render(){
+        
+
     return (
         <div>
             <div className={s.space_bg_img}></div>
@@ -27,16 +47,17 @@ function Section_1() {
             <section className={s.intro}>
                 <div className={s.intro_profile}>
 
-                    <h1  className={s.introduction}>Hi, I'm Dmitriy Bondarenko<br />Frontend Developer</h1>
+                    <h1 id="intro" className={s.introduction}>Hi, I'm Dmitriy Bondarenko<br />Frontend Developer</h1>
 
-                    <button className={s.intro_btn}>Let's get to know each other</button>
-                    <div className={s.intro_arrow}>
+                    <button id="action_btn" className={s.intro_btn}>Let's get to know each other</button>
+                    <div id="arrow" className={s.intro_arrow}>
                         <img className={s.intro_arrow_svg} src={arrowIcon} alt="Arrow" />
                     </div>
                 </div>
             </section>
         </div >
     )
+    }
 }
 
 export default Section_1
